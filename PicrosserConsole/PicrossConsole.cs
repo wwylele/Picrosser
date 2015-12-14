d68217a3902d4da485cbc7f8161b09063076401d
wwylele <wwylele@gmail.com>
2015-12-14 08:38:03 +0200

    Add Console Application

---
+++ PicrosserConsole/PicrossConsole.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Picrosser;
+namespace PicrosserConsole {
+    class PicrossConsole {
+        static void Main(string[] args) {
+            string text = "", line;
+            int sec = 0;
+            while(sec < 2) {
+                line = Console.ReadLine();
+                text += line + "\n";
+                if(line.Equals("")) ++sec;
+            }
+            Question question;
+            try {
+                question = new Question(text);
+            } catch(ArgumentException) {
+                Console.WriteLine("Invalid Picross!");
+                return;
+            }
+            Solver solver = new Solver();
+            foreach(var step in solver.Solve(question)) ;
+            for(int y = 0; y < question.Height; ++y) {
+                for(int x = 0; x < question.Width; ++x) {
+                    switch(solver.GetPixelState(x, y)) {
+                    case Solver.PixelStateEnum.UNKNOWN:
+                        Console.Write(" ?");
+                        break;
+                    case Solver.PixelStateEnum.ON:
+                        Console.Write(" *");
+                        break;
+                    case Solver.PixelStateEnum.OFF:
+                        Console.Write("  ");
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            switch(solver.Result) {
+            case Solver.ResultEnum.CONTRADICTORY:
+                Console.Write("Found a contradiction!");
+                break;
+            case Solver.ResultEnum.INDEFINITE:
+                Console.Write("Failed to find a unique solution!");
+                break;
+            case Solver.ResultEnum.FINISHED:
+                Console.Write("Finished!");
+                break;
+            }
+        }
+    }
+}
---
+++ PicrosserConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("PicrosserConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PicrosserConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("02488c1d-d0c3-42ce-b8c7-d929cc349baa")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
